1fee56f6cf7ecf68d3798a8d57c854dae445f752
Luis Fernando Ramirez <luisf350@gmail.com>
2021-11-23 16:39:38 -0500

    Agregar archivos de proyecto.

---
+++ Servicios.Api.Libreria/Controllers/LibreriaServicioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Servicios.Api.Libreria.Core.Entities;
+using Servicios.Api.Libreria.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Servicios.Api.Libreria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibreriaServicioController : ControllerBase
+    {
+        private readonly IAutorRepository _autorRepository;
+
+        public LibreriaServicioController(IAutorRepository autorRepository)
+        {
+            _autorRepository = autorRepository;
+        }
+
+        [HttpGet("autores")]
+        public async Task<ActionResult<IEnumerable<Autor>>> GetAutores()
+        {
+            var autores = await _autorRepository.GetAutores();
+
+            return Ok(autores);
+        }
+    }
+}
---
+++ Servicios.Api.Libreria/Repository/IAutorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Servicios.Api.Libreria.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Servicios.Api.Libreria.Repository
+{
+    public interface IAutorRepository
+    {
+        Task<IEnumerable<Autor>> GetAutores();
+    }
+}
